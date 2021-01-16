0977b5f995828c2b7d124adc155f7362a0c4e22a
giannis-koulouris <gianiskoulouris@outlook.com>
2021-01-17 01:43:05 +0200

    progress 16-01-2021

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {Switch,Route} from 'react-router-dom';
 
-import Homepage from './pages/homepage/homepage.component.jsx';
+import HomePage from './pages/homepage/homepage.component.jsx';
+
+
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+      </Switch>
+      {/* <Homepage/> */}
     </div>
   );
 }
